253dbf1045189d783035bf84fc77e26e0d2cd692
gaborradnai <gabesz.radnai@gmail.com>
2025-11-24 12:55:15 -0800

    Update /feature greeting

--- HelloDevOpsApp/Program.cs
+++ HelloDevOpsApp/Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello DevOps!");
-app.MapGet("/feature", () => "Hello DevOps from feature branch!");
+app.MapGet("/feature", () => "Hello DevOps_V2 from feature branch!");
 
 app.Run();
